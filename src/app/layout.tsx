2868fbff99ee758669912ee71af21a2c3f4f6c31
agusmoles <agusmoles@gmail.com>
2026-05-29 13:07:38 +0000

    chore: adds analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col bg-zinc-950 text-zinc-100">
         {children}
       </body>
+      <GoogleAnalytics gaId="G-6BYZVE3TZ7" />
     </html>
   );
 }
